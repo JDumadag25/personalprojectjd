83ab2bfefaf8cd76755a5b818a405d3c3bf08257
Justin Dumadag <justin.dumadag@gmail.com>
2024-04-10 17:06:51 -0400

    Small data & add tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const data = {
+    cars: [
+        {
+            make: 'Nissan',
+            model: 'Sentra',
+            year: '2023',
+            color: 'Silver',
+        },
+        {
+            make: 'Nissan',
+            model: 'Rouge',
+            year: '2020',
+            color: 'Black',
+        },
+        {
+            make: 'Lexus',
+            model: 'IS 300',
+            year: '2022',
+            color: 'Blue',
+        },
+        {
+            make: 'Honda',
+            model: 'Accord',
+            year: '2023',
+            color: 'Silver',
+        },
+        {
+            make: 'Toyota',
+            model: 'Camry',
+            year: '2000',
+            color: 'White',
+        },
+    ],
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cars, setCars] = useState(data.cars);
+
+    return (
+        <>
+            <div>
+                <h1 className="text-3xl font-bold underline">Car Inventory</h1>
+            </div>
+            <div>
+                <div
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                    }}
+                >
+                    <div style={{ flex: 1 }}>Make</div>
+                    <div style={{ flex: 1 }}>Model</div>
+                    <div style={{ flex: 1 }}>Year</div>
+                    <div style={{ flex: 1 }}>Color</div>
+                </div>
+                {cars
+                    .sort((a, b) => a.id - b.id)
+                    .map((car, i) => {
+                        return (
+                            <div
+                                key={`car-${i}`}
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                }}
+                            >
+                                <div style={{ flex: 1 }}>{car.make}</div>
+                                <div style={{ flex: 1 }}>{car.model}</div>
+                                <div style={{ flex: 1 }}>{car.year}</div>
+                                <div style={{ flex: 1 }}>{car.color}</div>
+                            </div>
+                        );
+                    })}
+            </div>
+        </>
+    );
 }
 
 export default App;
